0e988811760082f04e92d63a2f5b807a601ffd74
fried <fried@ee0afd41-7e6f-42df-9d23-2503c6f95a77>
2018-02-18 23:02:19 +0000

    Added new script.

---
+++ src/compare-force.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+#
+# Compares the force between two files.  Used to compare forceout.txt from chimes_md to the
+# force.txt file produced by lsq-new-md-fmt.py
+#
+import sys
+import math
+
+af = open(sys.argv[1], "r").readlines()
+bf = open(sys.argv[2], "r").readlines()
+
+err = 0
+
+nf = min( len(af), len(bf) )
+
+for i in range (0,nf):
+    aff = float(af[i])
+    bff = float(bf[i])
+    err += (aff - bff) * (aff - bff)
+
+err /= nf
+err = math.sqrt(err)
+
+print "RMS Error = %7.5f kcal/mol-Ang." % err
+
