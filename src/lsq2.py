0ee9127e21e2ba8cbf7f96b2979b8906c99f0c7e
fried <fried@ee0afd41-7e6f-42df-9d23-2503c6f95a77>
2018-10-08 21:14:19 +0000

    Added support for OWLQN LASSO algorithm.

--- src/lsq2.py
+++ src/lsq2.py
@@ -3,12 +3,15 @@ import sys
 import numpy
 import scipy.linalg
 import math as m
+import subprocess
+import os
 
 import argparse
 
 from numpy import *
 from numpy.linalg import lstsq
 from datetime import *
+from subprocess import call
 
 parser = argparse.ArgumentParser(description='Least-squares force matching based on output of chimes_lsq')
 
@@ -22,6 +25,75 @@ def is_number(s):
         return False
 
 
+def write_matrix_market(mat, f):
+# Print an array in matrix market format.
+# Matrix market uses column-oriented storage.
+# Only dense 1 and 2d arrays supported.    
+    ff = open(f, "w")
+    ff.write("%%MatrixMarket matrix array real general\n")
+    ff.write("%%Comment\n") 
+
+    if len(mat.shape) == 1:
+        ff.write(str(mat.shape[0]) + " " + "1\n")
+        for i in range(0, mat.shape[0]):
+            ff.write(str(mat[i])+"\n")
+    elif len(mat.shape) == 2:
+        ff.write(str(mat.shape[0]) + " " + str(mat.shape[1]) + "\n")
+        for j in range(0, mat.shape[1]):
+            for i in range(0, mat.shape[0]):
+                ff.write(str(mat[i,j])+"\n")
+
+
+def read_matrix_market(f):
+# Read an array in matrix market format.
+# Matrix market uses column-oriented storage.
+# Only dense 1 and 2d arrays supported.    
+    ff = open(f, "r")
+    dat=ff.readlines()
+
+    sz=dat[1].split()
+    szi = [int(sz[0]), int(sz[1])]
+
+    print "ARRAY SIZE = " + str(szi[0]) + " " + str(szi[1])
+
+    if szi[0] == 1:
+        ar = numpy.zeros(szi[1], dtype=float)
+        print "SHAPE: " + str(ar.shape[0])
+        for j in range(0, szi[1]):
+            ar[j] = float(dat[j+2])
+    else:
+        print "UNSUPPORTED ARRAY TYPE"
+        sys.exit(1)
+        
+    return ar
+
+def count_nonzero_vars(x):
+    np = 0
+    for i in xrange(0, len(x)):
+        if ( abs(x[i]) > 1.0e-05 ):
+            np = np + 1
+    return np
+
+def fit_owlqn(A,b):
+## Use the OWLQN fitting algorithm
+    nvars = np
+    print '! OWLQN algorithm for LASSO used'
+    print '! OWLQN alpha = ' + str(alpha_val)
+    write_matrix_market(A, 'Amm.txt')
+    write_matrix_market(b, 'bmm.txt')
+    path=sys.path[0]
+    exepath=path[:-3] + "contrib/owlqn/source/owlqn"
+    if os.path.exists(exepath):
+        command = exepath + " Amm.txt bmm.txt " + str(alpha_val) + " fit.txt -ls -tol 1.0e-05 -m 20"
+        print "Running " + command
+
+        os.system(command)
+        x = read_matrix_market("fit.txt")
+        return x
+    else:
+        print exepath + " does not exist"
+        sys.exit(1)
+    
 # Arguments supported by the lsq code.
 parser.add_argument("--algorithm", default='svd', help='fitting algorithm')
 parser.add_argument("--A", default='A.txt',help='A (derivative) matrix') 
@@ -150,7 +222,7 @@ if DO_WEIGHTING:
     for i in xrange(A.shape[0]):     # Loop over rows (atom force components)
         for j in xrange(A.shape[1]): # Loop over cols (variables in fit)
 	    weightedA[i][j] = A[i][j]*WEIGHTS[i]
-	weightedb[i] = b[i]*WEIGHTS[i]
+	    weightedb[i] = b[i]*WEIGHTS[i]
 	    
     
 
@@ -180,7 +252,7 @@ if algorithm == 'svd':
     for i in range(0,len(Dmat)):
         if ( abs(D[i]) > dmax ) :
             dmax = abs(D[i])
-        
+            
         for j in range(0,len(Dmat[i])):
             Dmat[i][j]=0.0
 
@@ -193,7 +265,7 @@ if algorithm == 'svd':
         if abs(D[i]) > eps:
             Dmat[i][i]=1.0/D[i]
             nvars += 1
-        
+            
     print "! eps (= eps_fac*dmax) =  ", eps        
     print "! SVD regularization factor = ", eps_fac
 
@@ -229,10 +301,7 @@ elif algorithm == 'lasso':
     reg = linear_model.Lasso(alpha=alpha_val,fit_intercept=False,max_iter=max_iter_num)
     reg.fit(A,b)
     x = reg.coef_
-    np = 0 
-    for i in xrange(0, len(x)):
-        if ( abs(x[i]) > 1.0e-05 ):
-            np = np + 1
+    np = count_nonzero_vars(x)
     nvars = np
 
 elif algorithm == 'lassocv':
@@ -241,10 +310,7 @@ elif algorithm == 'lassocv':
     print '! Lasso CV regression used'
     print '! Lasso CV alpha = ' + str(reg.alpha_)
     x = reg.coef_
-    np = 0 
-    for i in xrange(0, len(x)):
-        if ( abs(x[i]) > 1.0e-05 ):
-            np = np + 1
+    np = count_nonzero_vars(x)
     nvars = np
 
 elif algorithm == 'lassolars':
@@ -257,13 +323,15 @@ elif algorithm == 'lassolars':
     else:
     	reg = linear_model.LassoLars(alpha=alpha_val,fit_intercept=False,fit_path=False,verbose=True,max_iter=max_iter_num)
     	reg.fit(A,b)
+
     x = reg.coef_[0]
-    np = 0 
-    for i in xrange(0, len(x)):
-        if ( abs(x[i]) > 1.0e-05 ):
-            np = np + 1
+    np = count_nonzero_vars(x)
     nvars = np
 
+elif algorithm == 'owlqn':
+    x = fit_owlqn(A, b)
+    np = count_nonzero_vars(x)
+    nvars = np
 else:
     print "Unrecognized fitting algorithm" 
     exit(1)
@@ -285,7 +353,7 @@ print "! max abs variable = ",  max(abs(x))
 print "! number of fitting vars = ", nvars
 print "! Bayesian Information Criterion =  ", bic
 if args.weights !="None":
-	print '! Using weighting file: ',weights_file
+    print '! Using weighting file: ',weights_file
 print "!"
 
 
@@ -346,7 +414,7 @@ if POTENTIAL == "CHEBYSHEV" or POTENTIAL == "DFTBPOLY":
     if len(TMP) >= 4:
         if len(TMP) >= 5:
             SNUM_4B = int(TMP[4])
-        
+            
         SNUM_2B = int(TMP[2])
         
         #print "Expecting:"
@@ -404,13 +472,13 @@ if TOTAL_TRIPS > 0:
 
         if P1[4] != "EXCLUDED:":
             SNUM_3B +=  int(P1[4])
-        
+            
             TOTL = P1[6]
             ADD_LINES += 5
-        
+            
             for i in xrange(0, int(TOTL)):
                 ADD_LINES += 1
-            
+                
 # Figure out how many 4B parameters there are
 
 SNUM_4B   = 0
@@ -425,9 +493,9 @@ if TOTAL_QUADS > 0:
         if P1[7] != "EXCLUDED:":
 
             SNUM_4B +=  int(P1[7])
-        
+            
             TOTL = P1[9]
-        
+            
             ADD_LINES += 5
 
             for i in xrange(0,int(TOTL)):
@@ -456,11 +524,11 @@ for i in range(0,TOTAL_PAIRS):
         
     for j in range(0, int(SNUM_2B)):
         print `j` + " " + `x[i*SNUM_2B+j]`
-    
+        
     if FIT_COUL == "true":
         print "q_" + A1 + " x q_" + A2 + " " + `x[TOTAL_PAIRS*SNUM_2B + SNUM_3B + SNUM_4B + i]`
         COUNTED_COUL_PARAMS += 1
-    
+        
     print " "
 
 # TRIPLETS
@@ -476,7 +544,7 @@ if TOTAL_TRIPS > 0:
     TRIP_PAR_IDX = 0
     
     for t in xrange(0, int(TOTAL_TRIPS)):
-    
+        
         PREV_TRIPIDX = 0
 
         print "TRIPLETTYPE PARAMS:"
@@ -500,22 +568,22 @@ if TOTAL_TRIPS > 0:
             print "   PAIRS: " + V0 + " " + V1 + " " + V2 + " UNIQUE: " + UNIQ + " TOTAL: " + TOTL 
             print "     index  |  powers  |  equiv index  |  param index  |       parameter       "
             print "   ----------------------------------------------------------------------------"
-        
+            
             ADD_LINES += 3
-        
+            
             if(t>0):
                 ADD_PARAM += 1
-        
+                
             for i in xrange(0,int(TOTL)):
                 ADD_LINES += 1
                 LINE       = hf[ATOM_TRIPS_LINE+2+ADD_LINES].rstrip('\n')
                 LINE_SPLIT = LINE.split()
-                    
+                
                 print LINE + " " + `x[TOTAL_PAIRS*SNUM_2B + TRIP_PAR_IDX+int(LINE_SPLIT[5])]`
 
             TRIP_PAR_IDX += int(UNIQ)
             COUNTED_TRIP_PARAMS += int(UNIQ)
-                #print "COUNTED_TRIP_PARAMS", COUNTED_TRIP_PARAMS
+            #print "COUNTED_TRIP_PARAMS", COUNTED_TRIP_PARAMS
                 
         print ""
         
@@ -530,9 +598,9 @@ if TOTAL_QUADS > 0:
     QUAD_PAR_IDX = 0
 
     for t in xrange(int(TOTAL_QUADS)):
-    
+        
         PREV_QUADIDX = 0
-                
+        
         #print "ATOM_QUADS_LINE " + str(ATOM_QUADS_LINE+2+ADD_LINES)
 
         P1 = hf[ATOM_QUADS_LINE+2+ADD_LINES].split()
@@ -565,19 +633,19 @@ if TOTAL_QUADS > 0:
             print "   PAIRS: " + V0 + " " + V1 + " " + V2 + " " + V3 + " " + V4 + " " + V5 + " UNIQUE: " + UNIQ + " TOTAL: " + TOTL 
             print "     index  |  powers  |  equiv index  |  param index  |       parameter       "
             print "   ----------------------------------------------------------------------------"
-        
+            
             ADD_LINES += 3
-        
+            
             if(t>0):
                 ADD_PARAM += 1
-        
+                
             for i in xrange(0,int(TOTL)):
                 ADD_LINES += 1
                 LINE       = hf[ATOM_QUADS_LINE+2+ADD_LINES].rstrip('\n')
                 LINE_SPLIT = LINE.split()
 
                 UNIQ_QUAD_IDX = int(LINE_SPLIT[8])
-                    #print 'UNIQ_QUAD_IDX', str(UNIQ_QUAD_IDX)
+                #print 'UNIQ_QUAD_IDX', str(UNIQ_QUAD_IDX)
 
                 print LINE + " " + `x[TOTAL_PAIRS*SNUM_2B + COUNTED_TRIP_PARAMS + QUAD_PAR_IDX + UNIQ_QUAD_IDX]`
 
@@ -596,16 +664,16 @@ if FIT_POVER == "true":
     OVERCOORD_PARAMS = 1
 else:
     OVERCOORD_PARAMS = 0
-	
+    
 mapsfile=open(map_file,"r").readlines()
 
 print ""
 
 for i in range(0,len(mapsfile)):
-	print mapsfile[i].rstrip('\n')
-	
+    print mapsfile[i].rstrip('\n')
+    
 print ""
-			
+
 total_params = TOTAL_PAIRS * SNUM_2B + COUNTED_TRIP_PARAMS + COUNTED_QUAD_PARAMS + COUNTED_COUL_PARAMS + OVERCOORD_PARAMS 
 if (total_params != len(x)) and (total_params != (len(x)-1)) :
     sys.stderr.write( "Error in counting parameters")
@@ -619,7 +687,7 @@ if test_suite_run:
     for i in range(0,len(x)):
         phrase = `i` + " " + `x[i]` + '\n'
         test_suite_params.write(phrase)
-    test_suite_params.close()
+        test_suite_params.close()
 
 print "ENDFILE"		
 
@@ -628,7 +696,7 @@ print "ENDFILE"
 #for i in range(0,len(x)):
 #    print i,x[i]
 
-
+                
 
 
 
