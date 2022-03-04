208da3e10ad866f889b88c61265112091c92bf75
Larry Fried <fried1@llnl.gov>
2022-03-04 12:48:18 -0800

    Moved final md output into a separate function.

--- src/chimes_md.C
+++ src/chimes_md.C
@@ -41,15 +41,17 @@
 using namespace std;	
 	
 // Define function headers -- general
-
 static void read_input        (string & INFILE, JOB_CONTROL & CONTROLS, NEIGHBORS & NEIGHBOR_LIST);
 static void read_atom_types(ifstream &PARAMFILE, JOB_CONTROL &CONTROLS, int &NATMTYP, vector<string>& TMP_ATOMTYPE, vector<int>& TMP_NATOMTYPE, vector<int>& TMP_ATOMTYPEIDX, vector<double>& TMP_CHARGES,  vector<double>& TMP_MASS, vector<int> &TMP_SIGN) ;
 static void read_ff_params(ifstream &PARAMFILE, JOB_CONTROL &CONTROLS, vector<PAIR_FF>& FF_2BODY, CLUSTER_LIST& TRIPS, CLUSTER_LIST &QUADS, map<string,int> &PAIR_MAP, NEIGHBORS &NEIGHBOR_LIST, FRAME& SYSTEM, int NATMTYP, const vector<string>& TMP_ATOMTYPE, const vector<int>& TMP_ATOMTYPEIDX, vector<double> &TMP_CHARGES, vector<double> &TMP_MASS, const vector<int>& TMP_SIGN, map<int,string>& PAIR_MAP_REVERSE) ;
 static void print_ff_summary(const vector<PAIR_FF> &FF_2BODY, CLUSTER_LIST &TRIPS, CLUSTER_LIST &QUADS, const JOB_CONTROL &CONTROLS) ;
+static void final_output(FRAME &SYSTEM, THERMO_AVG &AVG_DATA, JOB_CONTROL &CONTROLS, NEIGHBORS &NEIGHBOR_LIST, ofstream &STATISTICS,
+												 CONSTRAINT &ENSEMBLE_CONTROL) ;
 
 // Define function headers -- MPI
 
-static void write_xyzv         (FRAME &SYSTEM, JOB_CONTROL &CONTROLS, CONSTRAINT &ENSEMBLE_CONTROL, THERMO_AVG &AVG_DATA, NEIGHBORS &NEIGHBOR_LIST, string filename, bool restart);
+static void write_xyzv(FRAME &SYSTEM, JOB_CONTROL &CONTROLS, CONSTRAINT &ENSEMBLE_CONTROL,
+											 THERMO_AVG &AVG_DATA, NEIGHBORS &NEIGHBOR_LIST, string filename, bool restart);
 static void read_restart_params(ifstream &COORDFILE, JOB_CONTROL &CONTROLS, CONSTRAINT &ENSEMBLE_CONTROL, THERMO_AVG &AVG_DATA, NEIGHBORS &NEIGHBORS, FRAME &SYSTEM) ;
 static void parse_ff_controls  (string &LINE, ifstream &PARAMFILE, JOB_CONTROL &CONTROLS ) ;
 static void read_coord_file(int index, JOB_CONTROL &CONTROLS, FRAME &SYSTEM, ifstream &CMPR_FORCEFILE) ;
@@ -1137,52 +1139,8 @@ int main(int argc, char* argv[])
 	 ////////////////////////////////////////////////////////////
 	   
   }//End big loop here.
-  
-  if (RANK==0)	
-  {
-		cout << "END SIMULATION" << endl;
 
-		TEMP_MASS = 0.0;
-	
-		for ( int a = 0; a < SYSTEM.ATOMS; a++ ) 
-			TEMP_MASS  += SYSTEM.MASS[a];
-
-
-		cout << "	Average temperature over run = " << fixed << setprecision(4) << right << AVG_DATA.TEMP_SUM  / CONTROLS.N_MD_STEPS << " K"   << endl;
-		cout << "	Average pressure    over run = " << fixed << setprecision(4) << right << AVG_DATA.PRESS_SUM / CONTROLS.N_MD_STEPS << " GPa" << endl;
-		
-		// Stress tensors are not calculated for all potentials.
-		double Pavg = (AVG_DATA.STRESS_TENSOR_SUM_ALL[0].X + AVG_DATA.STRESS_TENSOR_SUM_ALL[1].Y + AVG_DATA.STRESS_TENSOR_SUM_ALL[2].Z)/3.0/ CONTROLS.N_MD_STEPS ;
-		cout << "	Pressures from diagonal stress tensors over run: " << Pavg << endl;
-		cout << "	Average stress tensors over run: " << endl;
-		cout << "		sigma_xx: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[0].X/CONTROLS.N_MD_STEPS << " GPa" << endl;
-		cout << "		sigma_yy: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[1].Y/CONTROLS.N_MD_STEPS << " GPa" << endl;
-		cout << "		sigma_zz: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[2].Z/CONTROLS.N_MD_STEPS << " GPa" << endl; 
-		cout << "		sigma_xy: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[0].Y/CONTROLS.N_MD_STEPS << " GPa" << endl;
-		cout << "		sigma_xz: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[0].Z/CONTROLS.N_MD_STEPS << " GPa" << endl;
-		cout << "		sigma_yz: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[1].Z/CONTROLS.N_MD_STEPS << " GPa" << endl;	   
-
-		if ( SYSTEM.BOXDIM.IS_VARIABLE )
-			{
-				cout << "        Average volume over run = " << fixed << setprecision(4) << right << AVG_DATA.VOLUME_SUM / CONTROLS.N_MD_STEPS << " Ang.^3" << endl ;
-				cout << "        Average PV over run     = " << fixed << setprecision(4) << right << AVG_DATA.PV_SUM / CONTROLS.N_MD_STEPS  << " kcal/mol "  
-						 << endl ;
-				if ( ENSEMBLE_CONTROL.STYLE == "NPT-MTK" )
-					{
-						// Allows a check on pressure and volume fluctuation magnitude, which should be correctly reproduced
-						// by NPT-NTK algorithm.
-						// See Martyna, Tobias, Klein JCP 101, 4177(1994) Appendix A.					 
-						cout << "        Average of PV predicted by virial theorem = " << fixed << setprecision(4) << right <<
-							(CONTROLS.PRESSURE / GPa) * AVG_DATA.VOLUME_SUM / CONTROLS.N_MD_STEPS - Kb * CONTROLS.TEMPERATURE
-								 << " kcal/mol" << endl ;
-					}
-			}
-	 
-		// Write the final configuration to file.
-		write_xyzv(SYSTEM, CONTROLS, ENSEMBLE_CONTROL, AVG_DATA, NEIGHBOR_LIST, "output.xyz", false);
-			
-		STATISTICS.close();
-	} // if ( RANK == 0 ) 
+	final_output(SYSTEM, AVG_DATA, CONTROLS, NEIGHBOR_LIST, STATISTICS, ENSEMBLE_CONTROL) ;
 	
 	normal_exit() ;
 }       
@@ -2779,4 +2737,55 @@ static void subtract_force(FRAME &SYSTEM, JOB_CONTROL &CONTROLS)
 		ferr = fabs(ferr/SYSTEM.ATOMS);
 		cout << "Absolute energy error = " << ferr << " kcal/mol/atom \n" ;
 	}
+}
+
+static void final_output(FRAME &SYSTEM, THERMO_AVG &AVG_DATA, JOB_CONTROL &CONTROLS, NEIGHBORS &NEIGHBOR_LIST, ofstream &STATISTICS,
+												 CONSTRAINT &ENSEMBLE_CONTROL)
+// Final output at the end of an MD simulation.
+{
+  if (RANK==0)	
+  {
+		cout << "END SIMULATION" << endl;
+
+		double TEMP_MASS = 0.0;
+	
+		for ( int a = 0; a < SYSTEM.ATOMS; a++ ) 
+			TEMP_MASS  += SYSTEM.MASS[a];
+
+
+		cout << "	Average temperature over run = " << fixed << setprecision(4) << right << AVG_DATA.TEMP_SUM  / CONTROLS.N_MD_STEPS << " K"   << endl;
+		cout << "	Average pressure    over run = " << fixed << setprecision(4) << right << AVG_DATA.PRESS_SUM / CONTROLS.N_MD_STEPS << " GPa" << endl;
+		
+		// Stress tensors are not calculated for all potentials.
+		double Pavg = (AVG_DATA.STRESS_TENSOR_SUM_ALL[0].X + AVG_DATA.STRESS_TENSOR_SUM_ALL[1].Y + AVG_DATA.STRESS_TENSOR_SUM_ALL[2].Z)/3.0/ CONTROLS.N_MD_STEPS ;
+		cout << "	Pressures from diagonal stress tensors over run: " << Pavg << endl;
+		cout << "	Average stress tensors over run: " << endl;
+		cout << "		sigma_xx: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[0].X/CONTROLS.N_MD_STEPS << " GPa" << endl;
+		cout << "		sigma_yy: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[1].Y/CONTROLS.N_MD_STEPS << " GPa" << endl;
+		cout << "		sigma_zz: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[2].Z/CONTROLS.N_MD_STEPS << " GPa" << endl; 
+		cout << "		sigma_xy: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[0].Y/CONTROLS.N_MD_STEPS << " GPa" << endl;
+		cout << "		sigma_xz: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[0].Z/CONTROLS.N_MD_STEPS << " GPa" << endl;
+		cout << "		sigma_yz: " << AVG_DATA.STRESS_TENSOR_SUM_ALL[1].Z/CONTROLS.N_MD_STEPS << " GPa" << endl;	   
+
+		if ( SYSTEM.BOXDIM.IS_VARIABLE )
+			{
+				cout << "        Average volume over run = " << fixed << setprecision(4) << right << AVG_DATA.VOLUME_SUM / CONTROLS.N_MD_STEPS << " Ang.^3" << endl ;
+				cout << "        Average PV over run     = " << fixed << setprecision(4) << right << AVG_DATA.PV_SUM / CONTROLS.N_MD_STEPS  << " kcal/mol "  
+						 << endl ;
+				if ( ENSEMBLE_CONTROL.STYLE == "NPT-MTK" )
+					{
+						// Allows a check on pressure and volume fluctuation magnitude, which should be correctly reproduced
+						// by NPT-NTK algorithm.
+						// See Martyna, Tobias, Klein JCP 101, 4177(1994) Appendix A.					 
+						cout << "        Average of PV predicted by virial theorem = " << fixed << setprecision(4) << right <<
+							(CONTROLS.PRESSURE / GPa) * AVG_DATA.VOLUME_SUM / CONTROLS.N_MD_STEPS - Kb * CONTROLS.TEMPERATURE
+								 << " kcal/mol" << endl ;
+					}
+			}
+	 
+		// Write the final configuration to file.
+		write_xyzv(SYSTEM, CONTROLS, ENSEMBLE_CONTROL, AVG_DATA, NEIGHBOR_LIST, "output.xyz", false);
+			
+		STATISTICS.close();
+	} // if ( RANK == 0 )
 }
